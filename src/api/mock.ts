81505484d5c11ffd65e94dbfebac2a6770efb36c
hykBigHeart <3069479901@qq.com>
2024-08-23 17:59:17 +0800

    1. 其他模块数据引入

--- src/api/mock.ts
+++ src/api/mock.ts
@@ -2,6 +2,9 @@ import { axiosInstance } from '@/plugins/installAxios';
 // 列表
 export function getData(type: number | string) {
   // return axiosInstance.get(`/mock/getdata?type=${type}`);
-  if (type === 'video') return axiosInstance.get(`/mock/data.json?type=${type}`)
-  else return axiosInstance.get(`/mock/audio.json?type=${type}`)
+  if (type === 'video') return axiosInstance.get(`/mock/video.json?type=${type}`)
+  else if (type === 'text') return axiosInstance.get(`/mock/text.json?type=${type}`)
+  else if (type === 'audio') return axiosInstance.get(`/mock/audio.json?type=${type}`)
+  else if (type === 'transition') return axiosInstance.get(`/mock/video.json?type=${type}`)
+  else return axiosInstance.get(`/mock/image.json?type=${type}`)
 }
